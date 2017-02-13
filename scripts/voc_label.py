5dd2f35621d966fc4846a8297a2fa6ddce59c3f1
Kentaro Wada <www.kentaro.wada@gmail.com>
2017-02-13 13:57:29 +0000

    Generate label for person

--- scripts/voc_label.py
+++ scripts/voc_label.py
@@ -6,7 +6,7 @@ from os.path import join
 
 sets=[('2012', 'train'), ('2012', 'val'), ('2007', 'train'), ('2007', 'val'), ('2007', 'test')]
 
-classes = ["aeroplane", "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair", "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant", "sheep", "sofa", "train", "tvmonitor"]
+classes = ["person"]
 
 
 def convert(size, box):
